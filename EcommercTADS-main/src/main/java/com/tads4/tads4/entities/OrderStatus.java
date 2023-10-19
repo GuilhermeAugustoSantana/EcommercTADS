6a143adf84391814d0eee018baeac6e719cfafaa
Guilherme Santana <73754403+GuilhermeAugustoSantana@users.noreply.github.com>
2023-10-18 21:19:33 -0400

    Delete EcommercTADS-main directory

--- EcommercTADS-main/src/main/java/com/tads4/tads4/entities/OrderStatus.java
+++
@@ -1,7 +0,0 @@
-package com.tads4.tads4.entities;
-
-public enum OrderStatus {
-
-    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
-
-}
--- EcommercTADS-main/src/main/java/com/tads4/tads4/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tads4.tads4.repositories;
-
-import com.tads4.tads4.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository <Product, Long> {
-}
